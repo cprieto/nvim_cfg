5abab119aa61ac47ec619a56cb48862bce3f5b08
Cristian Prieto <me@cprieto.com>
2024-03-18 22:01:30 +0100

    Show day of the week at dashboard

--- lua/plugins/start.lua
+++ lua/plugins/start.lua
@@ -1,5 +1,13 @@
 return {
   "nvimdev/dashboard-nvim",
   config = true,
+  opts = {
+    theme = "hyper",
+    config = {
+      week_header = {
+        enable = true,
+      },
+    },
+  },
   event = { "VimEnter" },
 }
