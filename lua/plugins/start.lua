25185fdcb45722411ba5df1d7c238727340dd312
Cristian Prieto <me@cprieto.com>
2024-03-29 19:32:22 +0100

    More actions in the dashboard

--- lua/plugins/start.lua
+++ lua/plugins/start.lua
@@ -1,9 +1,19 @@
+local function new_file()
+  local filename = vim.fn.input("Filename: ")
+  vim.cmd("e " .. filename)
+end
+
 return {
   "nvimdev/dashboard-nvim",
   config = true,
   opts = {
     theme = "hyper",
     config = {
+      shortcut = {
+        { icon = "", desc = " Explorer ", key = "e", group = "DashboardShortCut", action = function() vim.cmd [[Telescope file_browser]] end, },
+        { icon = "", desc = " New ", key = "n", group = "DashboardShortCut", action = new_file, },
+        { icon = "󰗼", desc = " Quit ", key = "q", group = "DashboardShortCut", action = function() vim.cmd "quit" end, },
+      },
       week_header = {
         enable = true,
       },
