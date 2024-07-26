df59eee9f89cf757658966303f3491fe0f06ba64
Cristian Prieto <me@cprieto.com>
2024-07-26 22:14:37 +0200

    Add neotest

--- lua/plugins/scrollbar.lua
+++ lua/plugins/scrollbar.lua
@@ -1,5 +1,15 @@
 return {
   "petertriho/nvim-scrollbar",
   config = true,
+  opts = {
+    excluded_filetypes = {
+      "cmp_docs",
+      "cmp_menu",
+      "dashboard",
+      "noice",
+      "prompt",
+      "TelescopePrompt",
+    }
+  },
   event = { "BufReadPost", "BufNewFile" },
 }
