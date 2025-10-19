2c2b201d76f739c3a288a01368950e8943c55d3d
Cristian Prieto <me@cprieto.com>
2025-10-19 16:12:12 +0200

    Fix lua problems with lazydev

---
+++ lua/plugins/lua.lua
@@ -0,0 +1,11 @@
+return {
+  'folke/lazydev.nvim',
+  ft = 'lua',
+  opts = {
+    library = {
+      { path = 'snacks.nvim', words = { 'Snacks' } },
+      { path = 'mini.icons', words = { 'MiniIcon' } },
+      { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+    }
+  }
+}
