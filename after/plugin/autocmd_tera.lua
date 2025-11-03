82425f80d2bd2556db7c20984bfeb20219cd38f6
Cristian Prieto <me@cprieto.com>
2025-11-03 21:53:43 +0100

    Cleaning master

--- after/plugin/autocmd_tera.lua
+++
@@ -1,8 +0,0 @@
-vim.filetype.add({ extension = { tera = "tera" } })
-
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "tera",
-  callback = function(event)
-    vim.bo[event.buf].commentstring = "{# %s #}"
-  end,
-})
